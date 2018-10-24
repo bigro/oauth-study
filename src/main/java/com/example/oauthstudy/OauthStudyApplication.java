a7a4b427701c2e7ab5e79606f705e8d835bb7b1e
Yuuki Ooguro <kitkat.jaja@gmail.com>
2018-10-24 16:13:41 +0900

    first commit

---
+++ src/main/java/com/example/oauthstudy/OauthStudyApplication.java
@@ -0,0 +1,12 @@
+package com.example.oauthstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthStudyApplication.class, args);
+	}
+}
